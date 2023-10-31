95e5264b922c567b4fa012910efbb5ad853852fd
Santirs10 <santiagorosanchez@outlook.es>
2023-10-31 11:13:06 +0100

    funcion.js added

---
+++ funcion.js
@@ -0,0 +1,10 @@
+function calcularLetraDNI(numeroDNI) {
+  if (isNaN(numeroDNI) || numeroDNI <0 || numeroDNI > 999999999 {
+    return "Número de DNI inválido";
+   }
+
+  const letrasDNI = "TRWAGMYFPDXBNJZSQVHLCKE";
+  const indice = numeroDNI % 23;
+ 
+  return letrasDNI.charAt(indice);
+}
